3db9345a4b10fbf47aabc60dbbeb8d433b9a83c9
maheshwaghmare <mwaghmare7@gmail.com>
2020-01-04 06:54:21 +0530

    Initial commit.

---
+++ node_modules/hello-world-by-mahesh/README.md
@@ -0,0 +1,3 @@
+# NPM Hello World Package
+
+It perform the mathematical operations such as `addition`, `subtraction`, `multiplication` and `division`.
---
+++ node_modules/hello-world-by-mahesh/index.js
@@ -0,0 +1,19 @@
+/**
+ * Addition of two numbers.
+ */
+module.exports.add = (a, b) => a + b
+
+/**
+ * Subtraction of two numbers.
+ */
+module.exports.sub = (a, b) => a - b
+
+/**
+ * Multiplication of two numbers.
+ */
+module.exports.mul = (a, b) => a * b
+
+/**
+ * Dividation of two numbers.
+ */
+module.exports.div = (a, b) => a / b
